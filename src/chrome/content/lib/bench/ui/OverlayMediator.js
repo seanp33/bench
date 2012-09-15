2b57ee88fdbccc6ab72a776eb99610f7138691d7
Sean Monaghan <smonagan000@gmail.com>
2012-09-15 01:12:59 -0400

    crude test of sigma.js

--- src/chrome/content/lib/bench/ui/OverlayMediator.js
+++ src/chrome/content/lib/bench/ui/OverlayMediator.js
@@ -4,11 +4,11 @@ dojo.require('bench.ui.Mediator');
 dojo.require('bench.util.Util');
 
 /**
-   Mediator responsible for interfacing the overlay.xul and the services
+ Mediator responsible for interfacing the overlay.xul and the services
  */
 dojo.declare('bench.ui.OverlayMediator', bench.ui.Mediator, {
 
-    constructor:function(){
+    constructor:function() {
         this._initCommands();
     },
 
@@ -17,7 +17,16 @@ dojo.declare('bench.ui.OverlayMediator', bench.ui.Mediator, {
     },
 
     cmd_doThatSht:function() {
+        let newTab = Application.activeWindow.open(this.url("chrome://bench/content/test.html"));
+        newTab.events.addListener("load", function() {
+            newTab.focus();
+        });
         this._logger.debug('ok, now I\'m doing that sht! from OverlayMediator');
+    },
+
+    url:function(spec) {
+        var ios = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
+        return ios.newURI(spec, null, null);
     }
 
 });
