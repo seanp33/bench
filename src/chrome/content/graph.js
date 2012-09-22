9871f08735944a8c183432e36fce610d7cddb6c1
Sean Monaghan <smonagan000@gmail.com>
2012-09-22 00:32:07 -0400

    some sigma js eval

--- src/chrome/content/graph.js
+++ src/chrome/content/graph.js
@@ -1,6 +1,52 @@
 Components.utils.import("resource://bench.modules/Context.jsm");
 
-function onLoad(){
+var sigInst = null;
+
+function onLoad() {
     var app = Context.get('bench.App');
-    alert(app.echo('bench.App can echo?'));
+
+    var entityService = Context.get('bench.service.EntityService');
+    if (entityService == undefined) throw new Error('bench.service.EntityService was not found within context');
+
+    // create graph
+    entityService.mapGraph(
+        function(node) {
+            try {
+                sigInst.addNode(node.id, node);
+            } catch(e) {
+                console.error(e);
+            }
+        },
+
+        function(src, srcPort, dst, dstPort) {
+            try {
+                var id = src + ':' + srcPort + ' > ' + dst + ':' + dstPort;
+                sigInst.addEdge(src, dst);
+            } catch(e) {
+                console.error(e);
+            }
+        },
+
+        function() {
+            sigInst.draw();
+        }
+    );
+
+    sigInst = sigma.init(document.getElementById('sigma-example'))
+        .drawingProperties({
+            defaultLabelColor: '#fff',
+            defaultLabelSize: 14,
+            defaultLabelBGColor: '#fff',
+            defaultLabelHoverColor: '#000',
+            labelThreshold: 6,
+            defaultEdgeType: 'curve'
+        }).graphProperties({
+            minNodeSize: 0.5,
+            maxNodeSize: 5,
+            minEdgeSize: 1,
+            maxEdgeSize: 1
+        }).mouseProperties({
+            maxRatio: 4
+        });
+
 }
--- src/chrome/content/lib/bench/service/EntityService.js
+++ src/chrome/content/lib/bench/service/EntityService.js
@@ -16,6 +16,33 @@ dojo.declare('bench.service.EntityService', bench.Loggable, {
         this._store.open();
         this._store.sql(bench.service.EntityService.CREATE_TABLE);
         this._store.close();
+    },
+
+    mapGraph:function(nodeFunction, edgeFunction, completeFunction){
+        let self = this;
+        let handler = new bench.storage.ResultHandler(
+            function(aResultSet) {
+                for (let row = aResultSet.getNextRow();
+                     row;
+                     row = aResultSet.getNextRow()) {
+                    let src = row.getResultByName('src');
+                    let src_port = row.getResultByName('src_port');
+                    let dst = row.getResultByName('dst');
+                    let dst_port = row.getResultByName('dst_port');
+                    nodeFunction({id:src, label:src, x:Math.random(), y:Math.random()});
+                    nodeFunction({id:src, label:src, x:Math.random(), y:Math.random()});
+                    edgeFunction(src, src_port, dst, dst_port);
+                }
+            },
+
+            function(){
+                completeFunction();
+            }
+        );
+
+        this._store.open();
+        this._store.sql('select * from entities', handler);
+        this._store.close();
     }
 
 });
